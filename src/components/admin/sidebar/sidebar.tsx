d265e69bf788763876a08e9da80090e904c8516d
tm0e83 <tm0e83@gmail.com>
2026-04-10 17:34:05 +0200

    close admin sidebar when path changes

--- src/components/admin/sidebar/sidebar.tsx
+++ src/components/admin/sidebar/sidebar.tsx
@@ -8,14 +8,20 @@ import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { useMediaQuery } from '@/hooks';
 import { useAdminSidebar } from '@/providers';
 import Button from '@/components/button';
+import { usePathname } from 'next/navigation';
 
 export default function Sidebar({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
   const isDesktop = useMediaQuery('(min-width: 1024px)');
   const { isExpanded, isMinimized, setIsExpanded, toggleMinimized } = useAdminSidebar();
 
   useEffect(() => {
     setIsExpanded(isDesktop);
   }, [isDesktop, setIsExpanded]);
+
+  useEffect(() => {
+    setIsExpanded(false);
+  }, [pathname]);
 
   return (
     <aside className={clsx(styles.sidebar, isExpanded && styles.expanded, isMinimized && styles.minimized)}>
