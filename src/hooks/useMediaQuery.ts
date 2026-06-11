a3135888aea83d744bdc3f7390b43a36e5acec61
tm0e83 <tm0e83@gmail.com>
2026-06-11 11:33:56 +0200

    update node modules; fix media query hook

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -4,11 +4,12 @@ import { useState, useEffect } from 'react';
  * @param query - e.g.: (min-width: 1024px)
  */
 export default function useMediaQuery(query: string) {
-  const [matches, setMatches] = useState(false);
+  const [matches, setMatches] = useState(
+    () => typeof window !== 'undefined' ? window.matchMedia(query).matches : false
+  );
 
   useEffect(() => {
     const media = window.matchMedia(query);
-    setMatches(media.matches);
     const listener = () => setMatches(media.matches);
     media.addEventListener('change', listener);
     return () => media.removeEventListener('change', listener);
