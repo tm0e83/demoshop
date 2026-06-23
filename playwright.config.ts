5b091c839d5d5ad2b7ecc0b42376cb44f74e5f97
tm0e83 <tm0e83@gmail.com>
2026-06-23 10:29:02 +0200

    adjust test config

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,10 +70,10 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run build && npm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });
