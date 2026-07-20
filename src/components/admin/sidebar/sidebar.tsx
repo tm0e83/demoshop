82f980805215768ae02b11f4cc1cc280c7318a20
tm0e83 <tm0e83@gmail.com>
2026-07-20 20:17:20 +0200

    fix:  prevent admin sidebar from closing on view change (desktop)

--- src/components/admin/sidebar/sidebar.tsx
+++ src/components/admin/sidebar/sidebar.tsx
@@ -20,7 +20,9 @@ export default function Sidebar({ children }: { children: React.ReactNode }) {
   }, [isDesktop, setIsExpanded]);
 
   useEffect(() => {
-    setIsExpanded(false);
+    if (!isDesktop) {
+      setIsExpanded(false);
+    }
   }, [pathname, isDesktop, setIsExpanded]);
 
   return (
